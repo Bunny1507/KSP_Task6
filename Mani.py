d5833c9925dbd695c38dcb5ed2dbbc5c1d0bbf0c
Bunny1507 <91088145+Bunny1507@users.noreply.github.com>
2023-09-18 09:18:45 +0530

    Add files via upload

---
+++ Mani.py
@@ -0,0 +1,57 @@
+# task 6 :- Recreate the topics from todays class - print statement , 
+# for loop, if else loop , variables , comment , datatype ,
+#  changing the datatypes , string string methods,
+#   funtion with aruguments and parameter
+
+# https://forms.gle/W3KowR2VgSobMVoJ7
+
+
+# print statement
+
+print("hi hello!!")
+
+
+# for loop
+name = "Manikanta"
+for i in name:
+    print(i)
+
+# if else loop
+
+a=int(input())
+
+if a==18:
+    print("U are Eligible")
+else:
+    print("not eligible")
+
+
+# variables
+a=10
+b=20
+name="hello"
+
+# datatype & changing the datatypes
+
+num1 = 3
+num1= float(num1)
+print(type(num1))
+
+# string string methods
+
+
+print(name.upper())
+
+a= len(name)
+print(a)
+
+b = name[-1:0]
+print(b)
+ 
+def add(num1, num2):
+
+    sum=num1+num2
+    print(sum)
+
+add(12,12)
+
